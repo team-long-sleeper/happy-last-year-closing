d306fa4c98ee725a250ccccfa44efb56f115d077
charlie-maxst <mkkim@maxst.com>
2025-12-19 18:43:58 +0900

    feat: first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="w-full h-dvh bg-primary-diamond-gradient text-3xl font-extralight text-white px-4 flex items-center">
+      <div className="flex justify-between w-full">
+        <div className="-rotate-10 -translate-y-4">happy</div>
+        <div className="">last</div>
+        <div className="rotate-4 translate-y-3">year</div>
+        <div className="-rotate-6">closing!</div>
+      </div>
+    </div>
+  );
+}
