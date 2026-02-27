52a8b97ca162a412f4a302d664eeb3e0e1e21709
김민경 <105103167+sparrowscout@users.noreply.github.com>
2026-02-27 18:01:46 +0900

    feat: add-episode 페이지 unmount 시 전역스토어 reset

--- app/add-episode/page.tsx
+++ app/add-episode/page.tsx
@@ -3,22 +3,39 @@ import Header from '@components/title/Header';
 import AddImage from '@components/add-eposide/AddImage';
 import AddFriends from '@components/add-eposide/AddMates';
 import PrimaryButton from '@components/buttons/PrimaryButton';
-import { useState } from 'react';
+import { useEffect, useMemo } from 'react';
 import AddPlace from '@components/add-eposide/AddPlace';
 import AddDates from '@components/add-eposide/AddDates';
 import AddTitle from '@components/add-eposide/AddTitle';
+import useEpisodeDataStore from '@/stores/add-/episodeDataStore';
+import useImageMetaData from '@/stores/imageMetaDataStore';
 
 export default function AddEpisode() {
-  const [isReadyToAdd, setIsReadyToAdd] = useState<boolean>(false);
+  const { date, mates, place, title, resetEpisodeData } = useEpisodeDataStore();
+  const { resetMetadata } = useImageMetaData();
+
+  const isReadyToAdd = useMemo(() => {
+    return !!(date && mates && place && title);
+  }, [date, mates, place, title]);
+
+  useEffect(() => {
+    return () => {
+      resetEpisodeData();
+      resetMetadata();
+    };
+  }, []);
 
   return (
-    <div className="h-dvh">
+    <div className="h-dvh overflow-hidden">
       <Header title="에피소드 추가하기" />
-      <AddTitle />
-      <AddDates />
-      <AddPlace />
-      <AddImage />
-      <AddFriends />
+      <div className="overflow-y-scroll pb-52 h-full">
+        <AddTitle />
+        <AddDates />
+        <AddPlace />
+        <AddImage />
+        <AddFriends />
+      </div>
+
       <PrimaryButton isBottomBtn isDisabled={!isReadyToAdd}>
         추가하기
       </PrimaryButton>
--- components/add-eposide/AddTitle.tsx
+++ components/add-eposide/AddTitle.tsx
@@ -1,13 +1,17 @@
+import useEpisodeDataStore from '@/stores/add-/episodeDataStore';
 import { BookmarkIcon } from '@assets/icons';
 import Icon from '@components/common/Icon';
 
 export default function AddTitle() {
+  const { setTitle } = useEpisodeDataStore();
+
   return (
     <div className="flex items-center gap-4 pl-25.5 pr-5 pb-12">
       <Icon icon={BookmarkIcon} />
       <input
         className="text-2xl w-full placeholder:text-primary-sub outline-none text-text-default"
         placeholder="에피소드 제목"
+        onChange={(e) => setTitle((e.target as HTMLInputElement).value)}
       />
     </div>
   );
