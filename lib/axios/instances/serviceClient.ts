17e9800cc2bf78a17b9bb4c277dfe6c673eb6830
김민경 <105103167+sparrowscout@users.noreply.github.com>
2026-04-01 21:01:20 +0900

    chore: readdy to make filtering

--- lib/axios/instances/serviceClient.ts
+++ lib/axios/instances/serviceClient.ts
@@ -1,9 +1,11 @@
 import axios, { AxiosInstance } from 'axios';
+import * as qs from 'qs';
 
 export const serviceClient: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   timeout: 10_000,
   withCredentials: true,
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'repeat' }),
 });
 
 export default serviceClient;
